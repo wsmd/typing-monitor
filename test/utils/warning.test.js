6266d77ff1a46645f49a7694ae47d0a4133a4ca5
Waseem Dahman <dwaseem@icloud.com>
2017-11-21 23:12:21 -0500

    Add test for utils/warning

---
+++ test/utils/warning.test.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+
+import warning from '../../src/utils/warning';
+
+describe('utils/warning', () => {
+  it('works', () => {
+    const MESSAGE = 'message';
+    const preSpy = console.error;
+    const spy = jest.fn();
+    console.error = spy;
+    try {
+      warning(MESSAGE);
+      expect(spy).toBeCalledWith(MESSAGE);
+    } finally {
+      console.error = preSpy;
+    }
+  });
+
+  it('does not throw when console.error is not available', () => {
+    const realConsole = console;
+    Object.defineProperty(global, 'console', { value: {} });
+    try {
+      expect(() => warning('please do not throw')).not.toThrow();
+    } finally {
+      Object.defineProperty(global, 'console', { value: realConsole });
+    }
+  });
+});
