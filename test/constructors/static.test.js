cd94839f08081b5fcab271e404c990fcdd4e0188
Waseem Dahman <dwaseem@icloud.com>
2017-11-22 14:42:24 -0500

    Update StaticTypingMonitor tests

--- test/constructors/static.test.js
+++ test/constructors/static.test.js
@@ -1,47 +1,40 @@
-import StaticMonitor from '../../src/constructors/static';
+import StaticTypingMonitor from '../../src/constructors/static';
 
 jest.useFakeTimers();
 
-describe('StaticMonitor', () => {
-  it('works', () => {
-    const typingMonitor = new StaticMonitor({ wait: 1000 });
-    expect(typingMonitor).toBeInstanceOf(StaticMonitor);
-  });
-
+describe('StaticTypingMonitor', () => {
   it('runs callback once after a wait period', () => {
-    const TIMES_CALLED = 10;
     const WAIT = 500;
-
-    const typingMonitor = new StaticMonitor({ wait: WAIT });
-
     const callback = jest.fn();
+    const typingMonitor = new StaticTypingMonitor({ wait: WAIT });
 
-    for (let i = 0; i < TIMES_CALLED; i++) { // eslint-disable-line
-      typingMonitor.listen(callback);
-    }
+    typingMonitor.listen(callback);
+    jest.runTimersToTime(WAIT - 1);
+    typingMonitor.listen(callback);
 
+    expect(callback).toHaveBeenCalledTimes(1);
     expect(callback).toHaveBeenLastCalledWith(true);
+    callback.mockReset();
 
     jest.runAllTimers();
 
     expect(callback).toHaveBeenLastCalledWith(false);
-    expect(callback).toHaveBeenCalledTimes(2);
 
     expect(setTimeout.mock.calls[0][1]).toBe(WAIT);
-    expect(setTimeout.mock.calls).toHaveLength(TIMES_CALLED);
+    expect(setTimeout.mock.calls).toHaveLength(2);
   });
 
   it('throws on invalid options', () => {
     const calls = [
-      () => new StaticMonitor(),
-      () => new StaticMonitor({}),
-      () => new StaticMonitor({ wait: 'string' }),
+      () => new StaticTypingMonitor(),
+      () => new StaticTypingMonitor({}),
+      () => new StaticTypingMonitor({ wait: 'string' }),
     ];
     calls.forEach(call => expect(call).toThrow());
   });
 
   it('throws on invalid listener', () => {
-    const monitor = new StaticMonitor({ wait: 500 });
+    const monitor = new StaticTypingMonitor({ wait: 500 });
     expect(() => {
       monitor.listen('not a function');
     }).toThrow();
